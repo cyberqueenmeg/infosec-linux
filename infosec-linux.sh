87dd51bd034999ad740caf3418027c8b4076bfdb
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-03 20:27:12 -0700

    Update infosec-linux.sh

--- infosec-linux.sh
+++ infosec-linux.sh
@@ -5,19 +5,19 @@ packageManager() {
     echo "Before we start, the installer needs to know what package manager you use."
     echo "Please enter 1 if you use APT (Debian/Ubuntu and its derivatives), 2 if you use RPM (includes YUM and DNF, runs on RedHat/Fedora systems and their derivatives), 3 if you use Pacman (Arch Linux and its derivatives), and 4 if you use another package manager or a non-Linux system (includes macOS, Windows, and other Operating System types that don't use these package managers.)"
     read packageManager
-    if [ $packageManger == 1 ]; then
+    if [[ $packageManger == 1 ]]; then
         echo "Starting APT installation script"
         $(line)
         ./apt-installer.sh
-    elif [ $packageManager == 2 ]; then
+    elif [[ $packageManager == 2 ]]; then
         echo "Starting RPM installation script"
         $(line)
         ./rpm-installer.sh
-    elif [ $packageManager == 3 ]; then
+    elif [[ $packageManager == 3 ]]; then
         echo "Starting Pacman installation script"
         $(line)
         ./pacman-installer.sh
-    elif [ $packageManager == 4 ]; then
+    elif [[ $packageManager == 4 ]]; then
         echo "We are sorry, but we do not offer a script to support your system at this time."
         echo "You can contribute support for your package manager to InfoSec Linux by creating a pull request at https://github.com/mhowell11/infosec-linux"
         echo "You may also run InfoSec Linux by installing a Linux distro using APT, RPM, or Pacman into a virtual machine, although performance may be downgraded."    
