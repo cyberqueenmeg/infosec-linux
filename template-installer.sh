2792d24a926bf08bac4c3cdc3e4b6cbdf0b1930b
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-03 20:17:55 -0700

    Update template-installer.sh

--- template-installer.sh
+++ template-installer.sh
@@ -2,10 +2,6 @@
 
 # This file can also be run independently later on to add more labs to VirtualBox without going through the entire set-up process.
 
-#function to echo a line between segments
-line() {
-    echo "----------------------------------"
-}
-
-$(line)
+    
+echo "----------------------------------"
 echo "Congratulations! If you made it this far, the previous scripts worked successfully. Thank you for your help in testing the hypervisor installation portion of InfoSec Linux. Please add your name to CONTRIBUTORS.md under First Beta Testers of the VirtualBox Installation Script following the format of First Name Last Initial (GitHub Username(s)) using a pull request."
