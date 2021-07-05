40b4684a901ff6d3fde9e8d68b077248f071cf9a
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-05 15:28:51 -0700

    Update rpm-installer.sh

--- rpm-installer.sh
+++ rpm-installer.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
-
-#function to install the layer 2 hypervisor, VirtualBox
-installVirtualBox () {
-    #Check for VBox
-    VBoxCheck=$(whereis virtualbox)
-
-    if [[ $VBoxCheck == *"/usr/bin"* ]]; then
-        echo "VirtualBox is installed!"
-        echo "----------------------------------"
-        echo "We will now begin installing the InfoSec Linux templates that you want."
-        ./template-installer.sh
-    else
-        echo "VirtualBox is not installed"
-        echo "The script is now installing VirtualBox"
-        wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import - 
-        sudo dnf virtualbox-guest-additions --noconfirm
-        echo "----------------------------------"
-        echo "We will now begin installing the InfoSec Linux templates that you want."
-        ./template-installer.sh
-    fi
-}
-
 echo "Welcome to the InfoSec Linux RPM installer."
 echo "----------------------------------"
-$(installVirtualBox)
+
+#Check for VBox
+VBoxCheck=$(whereis virtualbox)
+
+if [[ $VBoxCheck == *"/usr/bin"* ]]; then
+    echo "VirtualBox is installed!"
+    echo "----------------------------------"
+    echo "We will now begin installing the InfoSec Linux templates that you want."
+    ./template-installer.sh
+else
+    echo "VirtualBox is not installed"
+    echo "The script is now installing VirtualBox"
+    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import - 
+    sudo dnf virtualbox-guest-additions --noconfirm
+    echo "----------------------------------"
+    echo "We will now begin installing the InfoSec Linux templates that you want."
+    ./template-installer.sh
+fi
