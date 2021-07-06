2fc3feb9cae9772a110c2dd8aa176e23a081cf91
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-05 20:47:33 -0700

    Update rpm-installer.sh

--- rpm-installer.sh
+++ rpm-installer.sh
@@ -14,7 +14,6 @@ else
     echo "VirtualBox is not installed"
     echo "The script is now installing VirtualBox"
     wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import - 
-    sudo dnf virtualbox-guest-additions --noconfirm
     echo "----------------------------------"
     echo "We will now begin installing the InfoSec Linux templates that you want."
     ./template-installer.sh
