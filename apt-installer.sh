05785f6a2d262b7d58111c48db3b13864494cdc7
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-05 17:13:39 -0700

    Update apt-installer.sh

--- apt-installer.sh
+++ apt-installer.sh
@@ -18,9 +18,9 @@ else
     echo "The script is now installing VirtualBox"
     wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add - 
     wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - 
-    sudo apt-get update --noconfirm
-    sudo apt-get install virtualbox-6.1 --noconfirm
-    sudo apt install virtualbox-ext-pack --noconfirm
+    sudo apt-get update
+    sudo apt-get install virtualbox-6.1 
+    sudo apt install virtualbox-ext-pack 
     echo "----------------------------------"
     echo "We will now begin installing the InfoSec Linux templates that you want."
     ./template-installer.sh
