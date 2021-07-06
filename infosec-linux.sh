b4869ed79af9d1540806b6bec511f411ee40f6c3
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-05 17:06:00 -0700

    Update infosec-linux.sh

--- infosec-linux.sh
+++ infosec-linux.sh
@@ -7,33 +7,19 @@ echo "Please note that some software is proprietary. All software is legally acq
 echo "----------------------------------"
 echo "Before we start the installation, the script needs to detect what package manager you are using and if the package manager is supported by InfoSec Linux."
 
-declare -A osInfo;
-osInfo[/etc/redhat-release]=yum
-osInfo[/etc/arch-release]=pacman
-#osInfo[/etc/gentoo-release]=emerge - hoping to add support for Gentoo later
-#osInfo[/etc/SuSE-release]=zypp - hoping to add SuSE support later
-osInfo[/etc/debian_version]=apt
-
-for f in ${!osInfo[@]}
-do
-    if [[ -f $f ]];then
-        packageManager=${osInfo[$f]}
-    fi
-done
-
-
-if [[ $packageManger == 'apt' ]]; then
+if [ -x "$(command -v apt-get)" ]; then 
     echo "Starting APT installation script"
     echo "----------------------------------"
     ./apt-installer.sh
-elif [[ $packageManager == 'yum' ]]; then
+elif [ -x "$(command -v dnf)" ]; then
     echo "Starting RPM installation script"
     echo "----------------------------------"
     ./rpm-installer.sh
-elif [[ $packageManager == 'pacman' ]]; then
+elif [ -x "$(command -v pacman)" ]; then
     echo "Starting Pacman installation script"
     echo "----------------------------------"
     ./pacman-installer.sh
+    
 else 
     echo "We are sorry, but we do not offer a script to support your system at this time."
     echo "You can contribute support for your package manager to InfoSec Linux by creating a pull request at https://github.com/mhowell11/infosec-linux"
