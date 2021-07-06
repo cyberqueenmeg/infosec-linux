5e85ed9fb2eafc2e79589f87b8fda99a7e1f7bfc
mhowell11 <62813469+mhowell11@users.noreply.github.com>
2021-07-06 11:38:34 -0700

    Update and rename rpm-installer.sh to fedora-installer.sh

--- fedora-installer.sh
+++ fedora-installer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Welcome to the InfoSec Linux RPM installer."
+echo "Welcome to the InfoSec Linux Fedora installer."
 echo "----------------------------------"
 
 #Check for VBox
@@ -13,7 +13,12 @@ if [[ $VBoxCheck == *"/usr/bin"* ]]; then
 else
     echo "VirtualBox is not installed"
     echo "The script is now installing VirtualBox"
-    wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | rpm --import - 
+    wget http://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo -P /etc/yum.repos.d/
+    rpm --import https://www.virtualbox.org/download/oracle_vbox.asc
+    dnf update
+    dnf install @development-tools
+    dnf install kernel-devel kernel-headers dkms qt5-qtx11extras  elfutils-libelf-devel zlib-devel
+    dnf install VirtualBox-6.1
     echo "----------------------------------"
     echo "We will now begin installing the InfoSec Linux templates that you want."
     ./template-installer.sh
