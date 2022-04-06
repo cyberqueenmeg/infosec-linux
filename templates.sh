f865271e67ea006db95f62bffa25d2572ed6c9fd
Megan Howell <62813469+cyberqueen-meg@users.noreply.github.com>
2022-04-06 10:02:13 -0700

    Update templates.sh

--- templates.sh
+++ templates.sh
@@ -3,44 +3,12 @@
 echo "Welcome to the InfoSec Linux Virtual Machine Setup Wizard!"
 echo ""
 
+remoterepo = "http://infoseclinux.me" # need file path - will add later
+
 declare -a selection
-echo "Custom Cybersecurity Distributions"
-echo "1 - Kali Linux (Generic/normal install)"
-echo "2 - Parrot OS"
-echo "3 - Black Arch (Full repository)"
-echo "4 - Fedora Security"
-echo "5 - SecurityOnion"
-echo "6 - pfSense"
-echo "7 - Caine Forensics"
-echo "8 - CSI Linux"
-echo "9 - Kali Linux (Trace Labs/Open Source Intelligence)"
-echo "10 - Honeydrive Web Application Penetration Testing"
-echo "11 - Security Dojo Hacking Distribution"
-#Add 'custom' InfoSec Linux pentesting distros for specific purposes - malware analysis, mobile, etc
-echo "Generic Operating Systems for everyday use or to use as targets"
-#add version numbers
-echo "Debian"
-echo "Ubuntu Desktop"
-echo "Ubuntu Server"
-echo "Fedora"
-echo "Fedora Server"
-echo "openSUSE"
-echo "CentOS"
-echo "Oracle Linux (Desktop)"
-echo "Oracle Linux Server"
-echo "Pear OS"
-echo "Solaris"
-echo "FreeBSD"
-echo "airyx"
 
-#Specialty operating systems
-echo "AcademiX"
-echo "Ubuntu Studio"
-echo "Fedora Robotics"
-echo "Scientific Linux"
-
-
-echo "Which template would you like to install?"
+echo "Would you like to install a cybersecurity OS or a generic OS right now?"
+echo "Select 1 for cybersecurity and select 2 for generic"
 
 read -r -p "Options: " selection
 selection=${selection:- 1}
@@ -49,26 +17,71 @@ selection=${selection:- 1}
 	case "${entry[@]}" in
 
 		1)
-		arch-chroot /mnt pacman -S plasma kde-applications --noconfirm 
+		echo "Which cybersecurity operating system would you like to install right now?"
+		declare -a cyberselection
+		echo "1 - Kali Linux (Generic/normal install)"
+		echo "2 - Parrot OS"
+		echo "3 - Black Arch (Full repository)"
+		echo "4 - Fedora Security"
+		echo "5 - SecurityOnion"
+		echo "6 - pfSense"
+		echo "7 - Caine Forensics"
+		echo "8 - CSI Linux"
+		echo "9 - Kali Linux (Trace Labs/Open Source Intelligence)"
+		echo "10 - Honeydrive Web Application Penetration Testing"
+		echo "11 - Security Dojo Hacking Distribution"
+		#Add 'custom' InfoSec Linux pentesting distros for specific purposes - malware analysis, mobile, etc
+
+		read -r -p cyberselection
+		cyberselection=${cyberselection:- 1}
+			for entry in $cyberselection ;do
+
+			case "${entry[@]}" in
+				1)
+				;;
+
+				2)
+				;;
 		;;
 		
 		2)
-		arch-chroot /mnt pacman -S xfce4 xfce4-goodies --noconfirm 
-		;;
+		echo "Which generic operating system would you like to install right now?"
+		declare -a genericselection
+		#add version numbers
+		echo "1 - Debian"
+		echo "2 - Ubuntu Desktop"
+		echo "3 - Ubuntu Server"
+		echo "4 - Fedora"
+		echo "5 - Fedora Server"
+		echo "6 - openSUSE"
+		echo "7 - CentOS"
+		echo "8 - Oracle Linux (Desktop)"
+		echo "9 - Oracle Linux Server"
+		echo "10 - Gentoo"
+		echo "11 - Manjaro Linux"
+		echo "12 - Arch Linux"
+		echo "13 - Endeavour OS"
+		echo "14 - Solus"
+		echo "15 - Zorin OS"
+		echo "16 - POP! OS"
+		echo "17 - Raspbian"
+		echo "18 - Pear OS"
+		echo "19 - Solaris"
+		echo "20 - FreeBSD"
+		echo "21 - airyx"
 		
-		3)
-		arch-chroot /mnt pacman -S gnome --noconfirm
-		arch-chroot /mnt systemctl enable gdm.service
-		;;
-		
-		4)
-		arch-chroot /mnt pacman -S cinnamon xterm --noconfirm 
-		;;
-		
-		5)
-		arch-chroot /mnt pacman -S budgie-desktop xterm --noconfirm 
-		;;
-		
-		6)
-		arch-chroot /mnt pacman -S mate mate-extra ttf-freefont --noconfirm 
+
+
+		read -r -p genericselection
+		genericselection=${genericselection:- 1}
+			for entry in $genericselection ;do
+
+			case "${entry[@]}" in
+				1)
+				;;
+
+				2)
+				;;
+
+				#etc
 		;;
