432289e3cbb8f51a34f309a447280ed14b5a070b
Megan Howell <62813469+cyberqueen-meg@users.noreply.github.com>
2022-04-26 10:09:02 -0700

    Update templates.sh

--- templates.sh
+++ templates.sh
@@ -28,8 +28,9 @@ selection=${selection:- 1}
 		echo "7 - Caine Forensics"
 		echo "8 - CSI Linux"
 		echo "9 - Kali Linux (Trace Labs/Open Source Intelligence)"
-		echo "10 - Honeydrive Web Application Penetration Testing"
+		echo "10 - Honeydrive Web Application Penetration Testing (Honeypot maker)"
 		echo "11 - Security Dojo Hacking Distribution"
+		echo "12 - Pentoo"
 		#Add 'custom' InfoSec Linux pentesting distros for specific purposes - malware analysis, mobile, etc
 
 		read -r -p cyberselection
@@ -82,9 +83,18 @@ selection=${selection:- 1}
 				;;
 				
 				10)
+				#user needs to run 'sudo apt update && apt upgrade' after download. It will take a while since it's based on Xubuntu 12.
+				wget https://downloads.sourceforge.net/project/honeydrive/HoneyDrive%203%20Royal%20Jelly%20edition/HoneyDrive_3_Royal_Jelly.ova?ts=gAAAAABiaBz_eLxLj3zXZSe_hb9HySo5Uwu5WXGb2eki2bnhFoOGbjYY_JD1oc5UTriKNw2BX8IGuPnKBVxQPC8LrMMYLTqcQg%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fhoneydrive%2Ffiles%2Flatest%2Fdownload
 				;;
 				
 				11)
+				#user needs to run 'sudo apt update && apt upgrade' after download.
+				wget https://downloads.sourceforge.net/project/websecuritydojo/Version_3.4.1/Dojo-3.4.1.ova?ts=gAAAAABiaB0j24e0pxLqcRhnkLLDMkuYzcD_W5jqAu_vic8i4B8Y6Hz1A5rLirpSyhopFuf55UFr1AAurWSVvl2wWB2GNJdFPA%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fwebsecuritydojo%2Ffiles%2Flatest%2Fdownload
+				;;
+				
+				12)
+				#user needs to run 'emerge --ask --oneshot sys-apps/portage' after download.
+				wget https://pentoo.osuosl.org/daily-autobuilds/Pentoo_Full_x86_hardened/pentoo-full-x86-hardened-2022.0_p20220312.iso
 				;;
 				
 				
@@ -107,16 +117,14 @@ selection=${selection:- 1}
 		echo "11 - Manjaro Linux"
 		echo "12 - Arch Linux"
 		echo "13 - Endeavour OS"
-		echo "14 - Arco Linux"
+		echo "14 - Arco Linux (Cinnamon)"
 		echo "15 - Parrot Home"
 		echo "16 - Solus"
 		echo "17 - Zorin OS"
 		echo "18 - POP! OS"
 		echo "19 - Raspbian"
-		echo "20 - Pear OS"
-		echo "21 - Solaris"
-		echo "22 - FreeBSD"
-		echo "23 - airyx"
+		echo "20 - FreeBSD"
+		echo "21 - airyx"
 		
 
 
@@ -125,11 +133,87 @@ selection=${selection:- 1}
 			for entry in $genericselection ;do
 
 			case "${entry[@]}" in
-				1)
+				1
+				wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.3.0-amd64-netinst.iso
 				;;
 
 				2)
+				wget https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso
 				;;
-
-				#etc
+				
+				3)
+				wget https://releases.ubuntu.com/20.04.4/ubuntu-20.04.4-live-server-amd64.iso
+				;;
+				
+				4)
+				wget https://download.fedoraproject.org/pub/fedora/linux/releases/35/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso
+				;;
+				
+				5)
+				wget https://download.fedoraproject.org/pub/fedora/linux/releases/35/Server/x86_64/iso/Fedora-Server-dvd-x86_64-35-1.2.iso
+				;;
+				
+				6)
+				wget https://download.opensuse.org/distribution/leap/15.3/iso/openSUSE-Leap-15.3-DVD-x86_64-Current.iso
+				;;
+				
+				7)
+				wget http://mirror.arizona.edu/centos/7.9.2009/isos/x86_64/CentOS-7-x86_64-Everything-2009.iso
+				;;
+				
+				8)
+				wget https://yum.oracle.com/ISOS/OracleLinux/OL8/u5/x86_64/OracleLinux-R8-U5-x86_64-dvd.iso
+				;;
+				
+				9)
+				wget https://yum.oracle.com/ISOS/OracleLinux/OL7/u9/x86_64/OracleLinux-R7-U9-Server-x86_64-dvd.iso
+				;;
+				
+				10)
+				https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20220424T170534Z/livegui-amd64-20220424T170534Z.iso
+				;;
+				
+				11)
+				wget https://download.manjaro.org/xfce/21.2.6/manjaro-xfce-21.2.6-220416-linux515.iso
+				;;
+				
+				12)
+				wget https://gitlab.archlinux.org/archlinux/arch-boxes/-/jobs/54317/artifacts/file/output/Arch-Linux-x86_64-virtualbox-20220426.54317.box
+				;;
+				
+				13)
+				wget https://github.com/endeavouros-team/ISO/releases/download/1-EndeavourOS-ISO-releases-archive/EndeavourOS_Apollo_22_1.iso
+				;;
+				
+				14)
+				wget https://sourceforge.net/projects/arcolinux-community-editions/files/cinnamon/arcolinuxb-cinnamon-v22.03.07-x86_64.iso/download
+				;;
+				
+				15)
+				wget http://deb.parrot.sh/parrot/iso/5.0/Parrot-home-5.0_amd64.iso
+				;;
+				
+				16)
+				wget https://mirrors.rit.edu/solus/images/4.3/Solus-4.3-Budgie.iso
+				;;
+				
+				17)
+				wget http://deb.parrot.sh/parrot/iso/zorin/Zorin-OS-16-Core-64-bit-r4.iso
+				;;
+				
+				18)
+				wget https://pop-iso.sfo2.cdn.digitaloceanspaces.com/22.04/amd64/intel/4/pop-os_22.04_amd64_intel_4.iso
+				;;
+				
+				19)
+				wget https://downloads.raspberrypi.org/rpd_x86/images/rpd_x86-2021-01-12/2021-01-11-raspios-buster-i386.iso
+				;;
+				
+				20)
+				wget https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/13.0/FreeBSD-13.0-RELEASE-amd64-bootonly.iso
+				;;
+				
+				21)
+				wget https://mirrors.nomadlogic.org/nightly/airyxOS_0.4.0pre2_f13_5030978304344064_amd64.iso.md5
+				;;
 		;;
